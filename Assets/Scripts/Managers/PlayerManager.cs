841f6c8a7c7e0524a10600257c3f64033ccafedf
DelingsDecodes <williamtimothytino2004@gmail.com>
2025-06-06 23:47:39 +0100

    Update PlayerManager.cs

--- Assets/Scripts/Managers/PlayerManager.cs
+++ Assets/Scripts/Managers/PlayerManager.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class PlayerManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public List<Move> moveHistory = new List<Move>();  // Stores all past moves
+    public Move currentMove;                           // Current selected move
+
+    //Call this when the player selects a move (e.g. by clicking a button).
+
+    public void SelectMove(Move selectedMove)
     {
-        
+        currentMove = selectedMove;
+        moveHistory.Add(selectedMove);
+
+        Debug.Log("Player selected move: " + selectedMove.name);
     }
 
-    // Update is called once per frame
-    void Update()
+
+    // Returns the last move the player made (null if none).
+  
+    public Move GetLastMove()
     {
-        
+        if (moveHistory.Count == 0)
+            return null;
+
+        return moveHistory[moveHistory.Count - 1];
+    }
+
+    // Clears all move history (for a new game).
+
+    public void ResetHistory()
+    {
+        moveHistory.Clear();
+        currentMove = null;
     }
 }
