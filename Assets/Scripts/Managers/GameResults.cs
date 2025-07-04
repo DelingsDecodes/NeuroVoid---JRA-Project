12c881d75b8b337a0804bc8c042140a670ec81ef
DelingsDecodes <williamtimothytino2004@gmail.com>
2025-07-04 23:33:00 +0100

    Results

---
+++ Assets/Scripts/Managers/GameResults.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class GameResults
+{
+    public static string playerFinalMove;
+    public static string aiFinalMove;
+}
