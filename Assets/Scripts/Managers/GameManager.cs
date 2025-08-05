e7e674c568f47ad86e270b8979ec37049c4b79d2
DelingsDecodes <williamtimothytino2004@gmail.com>
2025-08-05 22:26:13 +0100

    scenes

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -49,12 +49,20 @@ public class GameManager : MonoBehaviour
         uiManager.UpdateRoundCounter(currentRound, totalRounds);
         UnlockAllCards();
 
-      
-        if (!string.IsNullOrEmpty(GameResults.Instance.finalTaunt))
+        Debug.Log("Current Round: " + currentRound);
+
+        if (currentRound == 1)
+        {
+            Debug.Log("GameManager: Showing welcome taunt.");
+            GameResults.Instance.finalTaunt = "";
+            StartCoroutine(ShowDelayedTaunt("Welcome, challenger. Let’s see what you're made of..."));
+        }
+        else if (!string.IsNullOrEmpty(GameResults.Instance.finalTaunt))
         {
             Debug.Log("GameManager: Displaying AI taunt from previous round.");
-            StartCoroutine(ShowDelayedTaunt(GameResults.Instance.finalTaunt));
-            GameResults.Instance.finalTaunt = ""; 
+            string tauntToShow = GameResults.Instance.finalTaunt;
+            GameResults.Instance.finalTaunt = "";
+            StartCoroutine(ShowDelayedTaunt(tauntToShow));
         }
     }
 
