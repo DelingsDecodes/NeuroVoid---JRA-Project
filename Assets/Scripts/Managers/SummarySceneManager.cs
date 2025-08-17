d87f8e2b0abca9ec2f9bcf64e5d43e6ce1a8d6b5
DelingsDecodes <williamtimothytino2004@gmail.com>
2025-08-17 23:20:40 +0100

    Update SummarySceneManager.cs

--- Assets/Scripts/Managers/SummarySceneManager.cs
+++ Assets/Scripts/Managers/SummarySceneManager.cs
@@ -1,34 +1,55 @@
+// === SummarySceneManager.cs ===
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
-
+using System.Text;
 
 public class SummarySceneManager : MonoBehaviour
 {
+    [Header("UI References")]
     public TextMeshProUGUI summaryText;
     public TextMeshProUGUI resultText;
+    public TextMeshProUGUI historyText;
+    public Button restartButton;
+    public Button mainMenuButton;
 
     void Start()
     {
-        if (GameResults.Instance == null)
-        {
-            Debug.LogWarning("SummarySceneManager: GameResults instance not found.");
-            return;
-        }
-
-        if (summaryText != null)
-            summaryText.text = GameResults.Instance.GetAdvancedSummary();
-
       
         if (resultText != null)
             resultText.text = GameResults.Instance.GetFinalResult();
+
+       
+        if (summaryText != null)
+            summaryText.text = GameResults.Instance.GetAdvancedSummary();
+
+        
+        if (historyText != null)
+        {
+            var history = GameResults.Instance.GetFullHistory();
+            StringBuilder builder = new StringBuilder();
+
+            builder.AppendLine("<b>--- Full Round Log ---</b>");
+            foreach (var entry in history)
+            {
+                builder.AppendLine(entry);
+            }
+
+            historyText.text = builder.ToString();
+        }
+
+      
+        if (restartButton != null)
+            restartButton.onClick.AddListener(RestartGame);
+
+        if (mainMenuButton != null)
+            mainMenuButton.onClick.AddListener(ReturnToMainMenu);
     }
 
     public void RestartGame()
     {
-        if (GameResults.Instance != null)
-            GameResults.Instance.Reset();
-
+        GameResults.Instance.Reset();
         SceneManager.LoadScene("MainScene");
     }
 
